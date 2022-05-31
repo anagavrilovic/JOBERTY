cef7a9bb165b525885ae516f6312a63d6e4c58e6
Marija Klještan <marijakljestan@uns.ac.rs>
2022-05-31 10:38:54 +0200

    Initial project

---
+++ backend/src/main/java/com/joberty/backend/JobertyApplication.java
@@ -0,0 +1,13 @@
+package com.joberty.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobertyApplication.class, args);
+    }
+
+}
